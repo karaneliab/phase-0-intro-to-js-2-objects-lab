fe0793a3227fda1aa4b28d79b08ee31baabc975a
karaneliab <eliabkaran7884@gmail.com>
2023-12-15 09:00:47 +0300

    more than have passed

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 // Write your solution in this file!
+const employee = {
+   name : 'sam',
+   streetAddress : '11 Broadway',
+}
+console.log('first employee', employee)
+
+function updateEmployeeWithKeyAndValue(employee, key, value){
+    const updatedEmployee = { ...employee, [key]: value };{
+    return updatedEmployee;
+  }
+  
+}
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    const destructivelyUpdatedEmployee = (key,value );{
+        return entireUpdatedEmployee;
+    }
+}
+function deleteFromEmployeeByKey(employee, key){
+    const deletedEmployee = {...employee, [key]: "streeAdress"}; {
+        return deletedEmployee;
+    }
+}
+function deleteEmployeeByKey(employee, key){
+let newEmployee = (employee, 'key');
+  return newEmployee;
+}
+console.log("name");
+console.log(typeof newEmployee);
+
+function  destructivelyDeleteFromEmployeeByKey(employee, key) {
+    let newEmployee = (employee, 'name');
+    return newEmployee;
+}
+console.log(newEmployee[name]);
+console.log(typeof newEmployee);
+
+function DestructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    let destructivelyUpdatedEmployee = (key,value );
+        return entireUpdatedEmployee;
+    }
+console.log(destructivelyUpdatedEmployee);
